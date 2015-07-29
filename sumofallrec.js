5c84fd47fa896cb6c670e5839b720fd17d6dde24
Brett Lyons <blyons@students.naropa.edu>
2015-07-29 17:03:33 -0600

    Began work on map function

--- sumofallrec.js
+++ sumofallrec.js
@@ -1,12 +1,23 @@
-console.log("Sum: " + sum(1, 2, 3, 4, 5));
+console.log("Sum: " + sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
-console.log("Sum: " + sum(1 , 3, 4, 5, 6, 7, 9));
-function sum ( ) {
-  var inputArr = Array.prototype.slice.call(arguments);
-  function go(inputArr, output) {
+function sum ( inputArr ) {
+  function go( inputArr, output) {
     if ( inputArr.length == 0 ) { return output; };
 
     return go(inputArr.slice(1), output + inputArr[0]);
   }
-return go(inputArr, 0);
+return go( inputArr, 0 );
 }
+
+function map ( inFunct, inArr ) {
+  for ( var i = 0 ; i < inArr.length ; i++ ) {
+    return inFunct(inArr[i]);
+  }
+}
+
+function mult2 ( x ) {
+  return x * 2;
+}
+
+
+console.log(map(mult2, [1,2,3]));
