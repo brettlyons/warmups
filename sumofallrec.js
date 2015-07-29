03b05f93c3488e4d46ad34a10e38def2368a1a2e
Brett Lyons <blyons@students.naropa.edu>
2015-07-28 20:23:20 -0600

    got recursive sum working

--- sumofallrec.js
+++ sumofallrec.js
@@ -1,7 +1,8 @@
 console.log("Sum: " + sum(1, 2, 3, 4, 5));
 
+console.log("Sum: " + sum(1 , 3, 4, 5, 6, 7, 9));
 function sum ( ) {
-  var inputArr = arguments;
+  var inputArr = Array.prototype.slice.call(arguments);
   function go(inputArr, output) {
     if ( inputArr.length == 0 ) { return output; };
 
