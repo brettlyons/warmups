b590ab87813985d9503e06dcec8a8bcded568274
Brett Lyons <blyons@students.naropa.edu>
2015-07-30 13:18:47 -0600

    some work

---
+++ objReverseEachElement.js
@@ -0,0 +1,16 @@
+// A String is a primitive data type, although it sometimes
+// acts like a reference data type.
+//
+// A Number is a primitive datatype.
+//
+// An array is a reference datatype that contains many variables
+// (Of varying types)
+//
+// A method is a function declared as a property of an object
+// A function exists without association with any particular
+// object, and can be declared within another function.
+// functions can be thought of as a datatype
+// in that they can be passed to other functions, or returned
+// and the typeof foo (){}; returns that the type is a Function.
+//
+
---
+++ objprac.js
@@ -0,0 +1,38 @@
+/*
+ *. Create an object to represent a laptop. It should have state for whether or not it is `on`, where or not it is `asleep`, and `numberOfDaysSinceLastRestart`. Give the laptop behaviors like `turnOff` (which should turn it off (not asleep) and reset the count since it was last restarted).
+ *
+ *2. Create an object to represent a coffee mug. It should have state for being `full` as well as its `capacityInOz` and `color`. A coffee mug should be able to be emptied and filled, but have no other behaviors.
+ */
+
+var lapObj = {
+  isOn: false,
+  isAsleep: false,
+  numberOfDaysSinceLastRestart: 0,
+
+  turnOff: function ( ) {
+    this.isOn = false;
+    this.numberOfDaysSinceLastRestart = 0;
+  },
+
+  toggleSleep: function () {
+    this.isAsleep = !this.isAsleep;
+  }
+};
+
+var coffeeMug = {
+  full: false,
+  capacityInOz: 8,
+  color: 'blue',
+
+  fill: function () {
+    this.full = true;
+  },
+
+  empty: function () {
+    this.full = false;
+  }
+};
+
+console.log(lapObj.isAsleep);
+lapObj.toggleSleep();
+console.log(lapObj.isAsleep);
