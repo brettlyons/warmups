dad94e7f834977fbb3ab5be102eba4fb786c8b97
Brett Lyons <blyons@students.naropa.edu>
2015-08-04 13:24:15 -0600

    added iterative function vers. to elem.js

--- elem.js
+++ elem.js
@@ -1,18 +1,31 @@
-var elem = function( xs, y ){
-  function go( xs, y, output) {
-
-//    console.log(xs + " " + y + " " + output + " " + xs[0] + " " + (xs[0] == y));
-
-    if (xs.length == 0 || output == true ) { return output; }
-  return go(xs.slice(1), y, (xs[0] == y));
+var elem = function ( array, target ) {
+  for ( var i = 0 ; i < array.length ; i++ ) {
+    if ( array[i] == target ) { return true; }
   }
-return go( xs, y, false )
+return false;
 }
-console.log(elem([1,2,3], 3));
-console.log(elem([1,2,3], 2));
-console.log(elem([1,2,3], 4));
-console.log(elem([1,2,3,4,5,34,62,43,32,5,3,4, 99], 99));
 
+
+/*
+ *var elem = function( xs, y ){
+ *  function go( xs, y, output) {
+ *
+ *    console.log(xs + " " + y + " " + output + " " + xs[0] + " " + (xs[0] == y));
+ *
+ *    if (xs.length == 0 || output == true ) { return output; }
+ *  return go(xs.slice(1), y, (xs[0] == y));
+ *  }
+ *return go( xs, y, false )
+ *}
+ */
+console.log(elem([1,2,3], 3));
+console.log("^ true ^ ");
+console.log(elem([1,2,3], 2));
+console.log("^ true ^ ");
+console.log(elem([1,2,3], 4));
+console.log("^ false ^ ");
+console.log(elem([1,2,3,4,5,34,62,43,32,5,3,4, 99], 99));
+console.log("^ true ^ ");
 
 
 
