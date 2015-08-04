8d339e5a2620f24e9da3307c2ce4434d4f97fbb9
Brett Lyons <blyons@students.naropa.edu>
2015-08-04 13:39:38 -0600

    added include function to elem.js -- success

--- elem.js
+++ elem.js
@@ -1,16 +1,24 @@
-var elem = function ( array, target ) {
-  for ( var i = 0 ; i < array.length ; i++ ) {
-    if ( array[i] == target ) { return true; }
+var include = function ( array, target ) {
+  for ( idx in array ) {
+    if ( array[idx] === target ) { return true; }
   }
 return false;
 }
+
+var a = [1,2,3,4,5];
+console.log( include( a, 3 )); //true
+console.log( include( a, 7 )); //false
+console.log( include( a, "3")); //false
+console.log( include( a, {a: 1})); //false
+
+
 
 
 /*
  *var elem = function( xs, y ){
  *  function go( xs, y, output) {
  *
- *    console.log(xs + " " + y + " " + output + " " + xs[0] + " " + (xs[0] == y));
+ *    console.log(xs + "\ninputArray, xs ^" + y + "\ntarget, y ^ " + output + "\noutput ^ " + xs[0] + "\n0th element of xs ^" + (xs[0] == y));
  *
  *    if (xs.length == 0 || output == true ) { return output; }
  *  return go(xs.slice(1), y, (xs[0] == y));
@@ -18,14 +26,14 @@ return false;
  *return go( xs, y, false )
  *}
  */
-console.log(elem([1,2,3], 3));
-console.log("^ true ^ ");
-console.log(elem([1,2,3], 2));
-console.log("^ true ^ ");
-console.log(elem([1,2,3], 4));
-console.log("^ false ^ ");
-console.log(elem([1,2,3,4,5,34,62,43,32,5,3,4, 99], 99));
-console.log("^ true ^ ");
+//console.log(elem([1,2,3], 3));
+//console.log("^ true ^ ");
+//console.log(elem([1,2,3], 2));
+//console.log("^ true ^ ");
+//console.log(elem([1,2,3], 4));
+//console.log("^ false ^ ");
+//console.log(elem([1,2,3,4,5,34,62,43,32,5,3,4, 99], 99));
+//console.log("^ true ^ ");
 
 
 
