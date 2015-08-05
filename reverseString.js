f4ad922240ffbdc8d60a9e877cf70f742818b4f9
Brett Lyons <blyons@students.naropa.edu>
2015-08-05 13:43:47 -0600

    Stripped out reverseInPlace function for re-writting

--- reverseString.js
+++ reverseString.js
@@ -9,17 +9,10 @@
 //console.log(reverseString('foof')); // 'foof'
 //console.log(reverseString('stringey')); // 'yegnirts'
 
-// this is not working currently -- it does not mutate the inString.
-function reverseInPlace ( inString ) {
-  var tmpString = "";
-  for ( var i = inString.length ; i >= 0 ; i-- ) {
-    tmpString += inString[i];
-  }
-  for ( var i = tmpString.length, j = 0 ; i >= 0 ; i--, j++ ) {
-    inString[i] = tmpString[j];
-    console.log(inString);
-  }
+function reverseInPlace ( inputString ) {
+
 }
+
 var myString1 = "Yellow Door";
 var myString2 = "green forest";
 reverseInPlace (myString1);
