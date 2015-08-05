e7b2aa0d3e96f27f503d37de076f71da40b887d2
Brett Lyons <blyons@students.naropa.edu>
2015-08-05 11:20:33 -0600

    Added while-loop vers of trim

--- newTrim.js
+++ newTrim.js
@@ -1,17 +1,44 @@
+/*
+ *function trim ( inString ) {
+ *  var firstNonSpaceIndex = 0;
+ *  var lastNonSpaceIndex = inString.length - 1;
+ *  for ( letterIndex in inString ) {
+ *    if ( inString[letterIndex] == ' ' ) { continue; }
+ *    firstNonSpaceIndex = letterIndex;
+ *    break;
+ *  }
+ *  for ( var i = lastNonSpaceIndex ; i >= 0; i-- ) {
+ *    if ( inString[i] == ' ' ) { continue; }
+ *    lastNonSpaceIndex = i;
+ *    return inString.substring(firstNonSpaceIndex, lastNonSpaceIndex);
+ *    break;
+ *  }
+ *  var output = "";
+ *  for ( var j = firstNonSpaceIndex ; j < lastNonSpaceIndex ; j++ ) {
+ *    output += inString[j];
+ *  }
+ *  return output;
+ *}
+ *
+ */
+
 function trim ( inString ) {
   var firstNonSpaceIndex = 0;
   var lastNonSpaceIndex = inString.length - 1;
-  for ( letterIndex in inString ) {
-    if ( inString[letterIndex] == ' ' ) { continue; }
-    firstNonSpaceIndex = letterIndex;
-    break;
+  var output = "";
+
+  while ( inString[firstNonSpaceIndex] == ' ' ||
+          inString[lastNonSpaceIndex] == ' ' ) {
+    if (inString[firstNonSpaceIndex] == ' ') { firstNonSpaceIndex++; }
+    if (inString[lastNonSpaceIndex] == ' ') { lastNonSpaceIndex--; }
   }
-  for ( var i = lastNonSpaceIndex ; i >= 0; i-- ) {
-    if ( inString[i] == ' ' ) { continue; }
-    lastNonSpaceIndex = i;
-    return inString.substring(firstNonSpaceIndex, lastNonSpaceIndex);
+  for ( var j = firstNonSpaceIndex ; j < lastNonSpaceIndex ; j++ ) {
+    output += inString[j];
   }
+  return output;
 }
+
+
 
 
 var theString = "   My String    Loves Spaces!!     ";
