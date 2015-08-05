a946a876c50c4be217bdef2958dd666bf75aaff4
Brett Lyons <blyons@students.naropa.edu>
2015-08-05 09:40:22 -0600

    modified elem.js, added toString array -> string converter

--- elem.js
+++ elem.js
@@ -1,25 +1,28 @@
 var include = function ( array, target ) {
-  for ( idx in array ) {
-    if ( typeof ( array[idx] ) == typeof ( target ) ) {
-      for ( jdx in target ) {
-        if ( target[jdx] == array[idx][jdx] ) { return true };
+  for ( element in array ) {
+    if ( 'object' == typeof ( array[element] ) ) {
+      for ( sndElement in array[element] ) {
+        if ( target[sndElement] == array[element][sndElement] &&
+             array[element].length == target.length) {
+            return true;
+        }
       }
     }
-  if (array[idx] === target ) { return true; }
+  if (array[element] === target ) { return true; }
   }
 return false;
 }
 
-
-
 var a = [1,2,3,4,5];
 var b = [1, [2, 3]];
+var c = [1, {a: 1}];
 console.log("true ?>> " + include( a, 3 )); //true
 console.log("false ?>> " + include( a, 7 )); //false
 console.log("false ?>> " + include( a, "3")); //false
 console.log("false ?>> " +  include( a, {a: 1})); //false
 console.log("true ?>> " +  include( b, [2, 3])); //true ?
-
+console.log("true ? >> " + include (c, {a:1}));
+console.log("false ? >> " + include([1,2,3,[4,5,6]], [4,5]));
 
 /*
  *var elem = function( xs, y ){
---
+++ toString.js
@@ -0,0 +1,11 @@
+var toString = function ( inputArray ) {
+  function toString ( inputArray, accum ) {
+    //console.log( "**DEBUG ** Accumualted output:" + accum);
+    if ( inputArray[0] == undefined ) { return accum.slice(0, accum.length-1);}
+    accum += inputArray[0] + ",";
+    return toString ( inputArray.slice(1), accum ) ;
+  }
+return toString( inputArray, "");
+}
+console.log(toString(["Hello", "there!"]));
+console.log(toString([1,2,3,4,5]));
