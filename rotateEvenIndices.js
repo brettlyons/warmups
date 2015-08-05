66ca9e4d3f347f886dcc1121d6c9c84fe0bbae23
Brett Lyons <blyons@students.naropa.edu>
2015-08-05 15:30:46 -0600

    from guiding challenges #5, rotates even indices

---
+++ rotateEvenIndices.js
@@ -0,0 +1,29 @@
+
+function rotateEvenIndices ( inString ) {
+  var stackOfChars = [];
+  var output = "";
+  for ( var i = 0 ; i < inString.length ; i++ ) {
+    if ( i % 2 == 0 ) {
+      stackOfChars.unshift(inString[i]);
+    }
+  }
+  stackOfChars.push(stackOfChars.shift()); // puts 0th element onto the end
+
+  for ( var i = 0 ; i < inString.length ; i++ ) {
+    ( i % 2 == 0 ) ? output +=  stackOfChars.pop() : output += inString[i];
+  }
+  return output;
+}
+
+var string = 'foobar';
+console.log("The f moves accross and then back to the front");
+console.log(string);
+var rotatedOnce = rotateEvenIndices(string);
+console.log(rotatedOnce);
+
+var rotatedTwice = rotateEvenIndices(rotatedOnce);
+console.log(rotatedTwice);
+
+var backToOriginal = rotateEvenIndices(rotatedTwice);
+console.log(string === backToOriginal); // true
+console.log(backToOriginal);
