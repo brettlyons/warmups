0028877c999716ed374a8497ccf4cc9c029c85df
Brett Lyons <blyons@students.naropa.edu>
2015-08-06 17:45:27 -0600

    added map and moarmap practices for HoFs.

---
+++ mapPractice.js
@@ -0,0 +1,11 @@
+var foxTypes = ['Red Fox', 'Arctic Fox', 'Fennec Fox', 'Kit Fox', 'Swift Fox'];
+
+function addThe ( inStr ) {
+  return inStr.map(titleize);
+}
+
+function titleize( aString ) {
+  return "the ".concat(aString);
+}
+
+console.log(addThe( foxTypes ));
