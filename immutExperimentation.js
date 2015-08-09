3588d223cd3999cab1b605ddaa2e4a53e5d79d43
Brett Lyons <blyons@students.naropa.edu>
2015-08-08 21:38:14 -0600

    Began experimenting with immutable.js in immutExperimentation.js

---
+++ immutExperimentation.js
@@ -0,0 +1,16 @@
+var Immutable = require('immutable');
+var map1 = Immutable.Map({a:1, b:2, c:3});
+var map2 = map1.set('b', 50);
+console.log(map1.get('b'));
+console.log(map2.get('b'));
+console.log(map1.get('b'));
+
+console.log (
+  Immutable.Range(1, Infinity)
+  .skip(1000)
+  .map(function ( n ) { return  -n; })
+  .filter(function ( n ) {return n % 2 === 0} )
+  .take(2)
+  .reduce(function (r, n) {return r * n}, 1)
+  );
+
