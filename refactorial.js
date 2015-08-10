c80be03fcdbd9fed4eaace32ee3aaffb7b7f38ba
Brett Lyons <blyons@students.naropa.edu>
2015-08-10 10:45:28 -0600

    working on PE # 20, JS might not represent for this

--- refactorial.js
+++ refactorial.js
@@ -6,10 +6,17 @@ var factorial = function( n ) {
   }
 return go( n-1, n );
 }
-
-console.log(factorial ( 100 ));
-factorial (171);
-
+var accum = []
+var hugeFactorial = factorial ( 100 );
+for ( var i = 0; i < 166 ; i++ ) {
+  accum.push( hugeFactorial / 10 );
+  hugeFactorial = hugeFactorial / 10;
+}
+console.log(hugeFactorial);
+console.log(accum);
+//console.log(factorial ( 100 ));
+//factorial (171);
+//factorial ( 15722 );
 //I read a post on stackoverflow about tail-recursion in JS
 //and it pretty much left me goin . . "whaaaaaaat?"
 //Basically, all of these functions return a function,
