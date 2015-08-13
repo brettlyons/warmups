87db65cf9db24800d13438d44d6d4e166735f3fc
Brett Lyons <blyons@students.naropa.edu>
2015-08-13 17:07:08 -0600

    Added goofy constructorception.js

---
+++ constructorception.js
@@ -0,0 +1,19 @@
+function Constructor ( param  ) {
+  var that = this;
+  return {
+    Constructor : function ( param  ) {
+      return {
+        Constructor: function ( param  ) {
+          console.log("I heard your encapsulation liked encapsulation, so I added some constructors to your constructors...",  param);
+
+        }
+
+      }
+    }
+  }
+
+}
+
+var construc = new Constructor ();
+
+console.log(construc.Constructor().Constructor('dawg'));
