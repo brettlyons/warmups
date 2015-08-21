254072362a265e15a7f6250d443a6acbf685ac1e
Brett Lyons <blyons@students.naropa.edu>
2015-08-21 10:41:58 -0600

    Edited subtractString.js

--- subtractString.js
+++ subtractString.js
@@ -1,4 +1,4 @@
-//var fjs = require('functional.js');
+var fjs = require('functional.js');
 //takes a string of arithmitic and returns the correct result
 
 //function subtractString ( string ) {
@@ -9,24 +9,43 @@
     //}, arrOfStrings[0] );
 //}
 
-function addString ( string ) {
-  var sum = 0;
-  var partition = 0;
-  for( var i = 0 ; i < string.length ; i++ ) {
-    if ( string[i] == "+") {
-      sum += Number(string.substring(partition, i));
-      partition = i+1;
-    }
-    if( i == string.length - 1 ) {
-      sum += Number(string.substring(partition, string.length));
-    }
-  }
-  return sum;
-};
-console.log(addString("3+5+7")); // 15
-console.log(addString("20+7+3")); // 30
-console.log(addString("235+8472+2")); //8709
-//console.log(subtractString('10-20-30'));
+
+//function subtractString( string ) {
+  //var difference = 0;
+  //var partition = 0;
+  //for( var i = 0 ; i < string.length ; i++ ) {
+    //if ( string[i] == "-") {
+      //difference -= Number(string.substring(partition, i));
+      //partition = i+1;
+    //}
+    //if( i == string.length - 1 ) {
+      //difference -= Number(string.substring(partition, string.length));
+    //}
+  //}
+  //return difference;
+//};
+//console.log(subtractString("3-5-7")); //
+//console.log(subtractString("20-7-3")); //
+//console.log(subtractString("235-8472-2")); //
+//console.log(subtractString('10-20-30')); // -40
+
+//function addString ( string ) {
+  //var sum = 0;
+  //var partition = 0;
+  //for( var i = 0 ; i < string.length ; i++ ) {
+    //if ( string[i] == "+") {
+      //sum += Number(string.substring(partition, i));
+      //partition = i+1;
+    //}
+    //if( i == string.length - 1 ) {
+      //sum += Number(string.substring(partition, string.length));
+    //}
+  //}
+  //return sum;
+//};
+//console.log(addString("3+5+7")); // 15
+//console.log(addString("20+7+3")); // 30
+//console.log(addString("235+8472+2")); //8709
 
 //function divideString ( string ) {
   //var arrOfStrings = string.split('/');
@@ -35,18 +54,25 @@ console.log(addString("235+8472+2")); //8709
 //}
 
 //console.log(divideString("10/20"));
-//this is a functional.js feature, a 'lambda expression'
+
 //var add = function ( x, y ){
   //return x + y;
 //};
 
 //var sum = fjs.reduce(add);
+//currying demo?
 
-//var subtract = function ( x, y ) {
-  //return x - y;
-//};
+//var nuString = "10-20-30";
 
-//var difference = fjs.reduce(subtract)
+var subtract = function ( x, y ) {
+  return x - y;
+};
+var difference = fjs.reduce("x, y => x - y");
 
-//console.log(difference([2, 3]));
+//var difference = fjs.reduce(subtract);
+
+//console.log(fjs.reduce(subtract, "10-20-30"));
+
+console.log(difference("10-20-30".split('-')));
+
 //console.log(sum([2, 3]));
