e97e5452b5033e8f4faf0f0493e766a24dfce12d
Brett Lyons <blyons@students.naropa.edu>
2015-08-25 09:10:41 -0600

    Added nostarmultiply.js untested.

---
+++ nostarmultiply.js
@@ -0,0 +1,12 @@
+
+function product(x, y) {
+  var output = 0;
+  for (var i = y ; i >= 0 : i--){
+    output += y;
+  }
+  return output;
+}
+
+console.log(product(10, 5)); // 50
+console.log(product(5, 5)); // 25
+
