7e13a1f6bbf6fe1bf086fd414b6063369fc90972
Brett Lyons <blyons@students.naropa.edu>
2015-08-29 08:48:26 -0600

    small changes to toDecimalFromBas

--- toDecimalFromBase7.js
+++ toDecimalFromBase7.js
@@ -11,7 +11,7 @@ function toDecimalFromBase(base, inString ) {
   for ( var i = inString.length - 1 ; i >= 0 ; i-- ) {
     powerOfSevenSeries.push(Math.pow(base, i));
   } // is there a better way to generate this?
-  console.log(powerSevenSeries);
+  console.log(powerOfSevenSeries);
   return ramda.sum(ramda.zipWith(mult, powerOfSevenSeries, tmpArray));
 } // pure function
   //even though it mutates a variable local to it's scope?
