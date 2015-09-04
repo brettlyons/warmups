0748cdd92bd260d835c0c523692a2fdea2428270
Brett Lyons <blyons@students.naropa.edu>
2015-09-04 10:08:11 -0600

    charCount(recursive) added.  cowConstructors.js added

---
+++ charCounts.js
@@ -0,0 +1,11 @@
+function charCounts(string) {
+  function go(storageObj, string) {
+    if(string !== '') { // definition of !base case (NOT base case)
+      storageObj[string[0]] = storageObj[string[0]] + 1 || 1;
+      return go(storageObj, string.slice(1)); // recursive call
+    }
+    return storageObj;  // base case
+  }
+  return go({}, string) //first call/invocation of go
+}
+console.log(charCounts("This is a string!!!"));
---
+++ cowConstructors.js
@@ -0,0 +1,33 @@
+function Cow(name, num) {
+  this.name = name;
+  this.num= num;
+  this.moo = function() {
+    return String("moooooooo I'm " + this.name + " moooooooo!!!!!!");
+  }
+}
+function CowFarm(cows) {
+  this.mooInOrder = function() {
+    output = '';
+    for(cow in cows) {
+      output += cows[cow].moo() + "\n";
+    }
+  return output.slice(0, -1); // gotta slice off the extra \n
+  }
+}
+
+//1. construct cows that can moo
+var bessy = new Cow("Bessy", 10);
+//console.log(bessy.moo());
+//moooooooo I'm Bessy moooooooo!!!!!!
+var mooey = new Cow("Mooey", 2);
+//console.log(mooey.moo());
+//moooooooo I'm Mooey moooooooo!!!!!!
+var chewy = new Cow("Chewy", 1);
+//console.log(chewy.moo());
+//moooooooo I'm Chewy moooooooo!!!!!!
+// 2. construct a farm full of cows, where all cows can be called on to moo in turn
+var farm = new CowFarm([bessy, mooey, chewy]);
+console.log(farm.mooInOrder());
+//moooooooo I'm Bessy moooooooo!!!!!!
+//moooooooo I'm Mooey moooooooo!!!!!!
+//moooooooo I'm Chewy moooooooo!!!!!!
