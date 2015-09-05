49cb106869a029d3f1e413dd1104b02018c82f73
Brett Lyons <blyons@students.naropa.edu>
2015-09-04 21:04:37 -0600

    finished palindomRecursive.js

---
+++ palindromeRecursive.js
@@ -0,0 +1,15 @@
+function palindrome (string) {
+  var spaceLessStr = string.replace(/\s/g, "");
+  function go(letterArr, letterStack) {
+    if (letterArr.length === 0) { return spaceLessStr === letterStack.join(''); }
+
+    letterStack.push(letterArr.pop());
+    return go(letterArr, letterStack)
+  }
+
+  return go(spaceLessStr.split(''), []);
+}
+
+console.log("true ?", palindrome("step on no pets")); // true
+console.log("false ?", palindrome("herpderp")); // false
+console.log("true ? ", palindrome('a man a plan a canal panama'));
