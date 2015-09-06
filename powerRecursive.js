b03ebb21c2382a768358fe7fa86f5afb721355ed
Brett Lyons <blyons@students.naropa.edu>
2015-09-06 17:06:20 -0600

    fewer lines in first definition

--- powerRecursive.js
+++ powerRecursive.js
@@ -1,6 +1,5 @@
 function power(base, exponent) {
-  if ( exponent == 1 ) { return base; }
-  return base * power(base, exponent-1);
+  return ( exponent == 1 ) ? base : (base * power(base, exponent-1));
 }
 
 console.log(power(2, 3));
