e1f48d771ce92ec71ccc9cf59fc311601a4ab665
Brett Lyons <blyons@students.naropa.edu>
2015-09-06 08:45:25 -0600

    Rewrote the function much simpler.

--- palindromeRecursive.js
+++ palindromeRecursive.js
@@ -1,15 +1,29 @@
-function palindrome (string) {
-  var spaceLessStr = string.replace(/\W/g, "");
-  function reverse(letterArr, letterStack) {
-    if (letterArr.length === 0) { return spaceLessStr === letterStack.join(''); }
+//function palindrome (string) {
+  //var spaceLessStr = string.replace(/\W/g, "");
+  //function reverse(letterArr, letterStack) {
+    //if (letterArr.length === 0) { return spaceLessStr === letterStack.join(''); }
 
-    letterStack.push(letterArr.pop());
-    return reverse(letterArr, letterStack)
-  }
+    //letterStack.push(letterArr.pop());
+    //return reverse(letterArr, letterStack)
+  //}
 
-  return reverse(spaceLessStr.split(''), []);
-}
+  //return reverse(spaceLessStr.split(''), []);
+//}
 
 console.log("true ?", palindrome("step on no pets")); // true
 console.log("false ?", palindrome("herpderp")); // false
-console.log("true ? ", palindrome('a man, a plan, a canal; panama'));
+console.log("true ? ", palindrome('a man, a plan, a canal; panama')); // true
+
+
+function palindrome(string) { // also handles 'odd' palindromes
+  var sanitizedArray = string.replace(/\W/g, "").split('');
+
+  function isPalindrome(arrayOfChrs) {
+    if(arrayOfChrs.length <= 1) { return true; }
+    if(arrayOfChrs.pop() != arrayOfChrs.shift()) { return false; }
+
+    return isPalindrome(arrayOfChrs);
+  }
+
+  return isPalindrome(sanitizedArray);
+}
