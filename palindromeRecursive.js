71200348f25dd9746ed3cfaa19691fa347063c0e
Brett Lyons <blyons@students.naropa.edu>
2015-09-06 08:46:48 -0600

    Removed commented out section

--- palindromeRecursive.js
+++ palindromeRecursive.js
@@ -1,20 +1,3 @@
-//function palindrome (string) {
-  //var spaceLessStr = string.replace(/\W/g, "");
-  //function reverse(letterArr, letterStack) {
-    //if (letterArr.length === 0) { return spaceLessStr === letterStack.join(''); }
-
-    //letterStack.push(letterArr.pop());
-    //return reverse(letterArr, letterStack)
-  //}
-
-  //return reverse(spaceLessStr.split(''), []);
-//}
-
-console.log("true ?", palindrome("step on no pets")); // true
-console.log("false ?", palindrome("herpderp")); // false
-console.log("true ? ", palindrome('a man, a plan, a canal; panama')); // true
-
-
 function palindrome(string) { // also handles 'odd' palindromes
   var sanitizedArray = string.replace(/\W/g, "").split('');
 
@@ -27,3 +10,7 @@ function palindrome(string) { // also handles 'odd' palindromes
 
   return isPalindrome(sanitizedArray);
 }
+
+console.log("true ?", palindrome("step on no pets")); // true
+console.log("false ?", palindrome("herpderp")); // false
+console.log("true ? ", palindrome('a man, a plan, a canal; panama')); // true
