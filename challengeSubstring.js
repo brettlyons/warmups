31f862cda8924e61dadced2edaab170073323a71
Brett Lyons <blyons@students.naropa.edu>
2015-09-05 19:20:48 -0600

    Added notes about some "how"

--- challengeSubstring.js
+++ challengeSubstring.js
@@ -3,8 +3,15 @@
 
 //find the substrings . . .
 //find the longest substring ...
+// filter for all substrings?
+// then pick longest ?
 function maxSubstring(string1, string2) {
   var output =[];
+// search for the first two letters through the entire 2nd string
+// then search for the substr of the first three letters through the entire second string . . .
+// etc . . .
+// then search for the substring of all letters EXCEPT the first letter of the first string . . .
+// then search for the substring of all letters except the first two letters of the first string . . .
 
   return output;
 }
