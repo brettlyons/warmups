d4697ba760e94021888b35fcf46492291f2ac5fe
Brett Lyons <blyons@students.naropa.edu>
2015-09-08 10:24:46 -0600

    fixed base case to use 1 instead of base

--- powerRecursive.js
+++ powerRecursive.js
@@ -16,7 +16,7 @@
 // recursion highly useful.
 
 function power( base, exponent ) {
-  return ( exponent == 1 ) ? base : (base * power(base, exponent-1));
+  return ( exponent == 0 ) ? 1 : (base * power(base, exponent-1));
 }
 
 console.log(power(2, 3));
@@ -36,7 +36,7 @@ function powerSturdy(base, exponent) {
     exponent = Math.abs(exponent);
   }
   if ( exponent == 0 ) { return 1; }
-  if ( exponent == 1 )  { return base; }
+
   return base * powerSturdy(base, exponent-1);
 }
 
