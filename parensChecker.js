d23accf889830b315758fc97ec62d4c5f92029e7
Brett Lyons <blyons@students.naropa.edu>
2015-09-11 09:59:14 -0600

    parensChecker.js bug fixed on string lengths of less than 2

--- parensChecker.js
+++ parensChecker.js
@@ -1,16 +1,15 @@
 function parensChecker(string) {
+  if (string.length == 1) { return false; }
   function go(string, stack) {
-    if (string.length == 0) {
-      return (stack.length < 1);
-    }
+    if (string.length == 0) { return (stack.length < 1); }
     if (string[0] == '(') { stack.push(1); }
-    if (string[0] == ')') { stack.pop(1); }
+    if (string[0] == ')') { stack.pop(); }
 
     return go(string.slice(1), stack);
   }
   return go(string, []);
 }
-
+console.log(parensChecker(')')); // -- returns true but should return false
 console.log(parensChecker('()(((())))')); //=> true
 console.log(parensChecker('())))(')); //=> false
 console.log(parensChecker(')(')); //=> false
