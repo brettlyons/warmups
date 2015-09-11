7df3f6cbda95d9aaefb32aba56a0241871932158
Brett Lyons <blyons@students.naropa.edu>
2015-09-11 09:40:30 -0600

    Added arraySum recursive solution to codewars kata

---
+++ arraySum.js
@@ -0,0 +1,16 @@
+console.log(arraySum([1,2]));
+console.log(arraySum([1,2,3]));
+console.log(arraySum([1,2,[1,2]]));
+
+function arraySum(array) {
+  function go(array, sum) {
+    if (array.length == 0) { return sum;  }
+    if (typeof array[0] === 'number') {  sum += array[0];  }
+    if (typeof array[0] === 'object') {  return go(array[0], sum);   }
+
+    return go(array.slice(1), sum);
+
+  }
+  return go(array, 0);
+
+}
