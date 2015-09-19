a2b22565e1e53c8e3fe5ac19393d916d55aa780c
Brett Lyons <blyons@students.naropa.edu>
2015-09-19 17:43:09 -0600

    Further work on kata-sum

--- kata-sum-of-mod-from-1-to-n.js
+++ kata-sum-of-mod-from-1-to-n.js
@@ -2,28 +2,50 @@
 // codewars kata
 // f(n=10, m=5) // returns 20 (1+2+3+4+0 + 1+2+3+4+0)
 
-console.log(f(10, 5), 'f(10, 5)', '20?'); // 20
-console.log(f(20, 20), 'f(20, 20)', '190?'); // 190
-console.log(f(15, 10), 'f(15, 10)', '60');// 60
-console.log(f(72234289, 89913873)); // ??
+// split the input, so one counter goes upwards towards the "next higher" mod zero
+// and the other goes downwards, towards the "next lower" mod zero.
+// then they can be joined for the total sum of one whole division
+// and the one that goes downwards can account for the remainder
 
 function f(n, m) {
-  var tmpSum = 0;
-  var sndTmpSum = 0;
-  var hasHitSecondZero = false;
-  var hasHitFirstZero = false;
-  var ones = function(x) { return x % 10; };
-  while (!hasHitSecondZero) {
-    tmpSum += n-- % m;
-    if (ones(n) % m == 0) {
-      sndTmpSum = tmpSum;
-      if (hasHitFirstZero) {
-        tmpSum = 0;
-        hasHitSecondZero = true;
-      }
-      hasHitFirstZero = true;
-    }
-    console.log('Run once', n, ' ones ', ones(n % m));
+  var wholeDivs = Math.floor(n / m);
+  var modRotateUpperSplitSum = 0;
+  var modRotateLowerSplitSum = 0;
+  // var tsArr = [];
+  for (var i = (n % m == 0) ? n - 1 : n ; i % m != 0 ; i--) {
+    modRotateLowerSplitSum += ((i % m));
   }
-  return sndTmpSum + tmpSum + (n * (m - 1) / 2);
+  if (n % m != 0) {
+    for (var i = (n % m == 0) ? n + 2 : n + 1 ; i % m != 0 ; i++) {
+      modRotateUpperSplitSum += ((i % m));
+    }
+  }
+  return (n  * (m - 1) / 2);
+  // return ((modRotateLowerSplitSum + modRotateUpperSplitSum) *
+  // /*((wholeDivs < 2) ?*/ wholeDivs + /*: wholeDivs - 1) +
+  // ((m - n == 0) ? 0 :*/  modRotateLowerSplitSum);
+  // // return ((modRotateLowerSplitSum + modRotateUpperSplitSum) * (wholeDivs - 1)) +
+  // //   modRotateLowerSplitSum;
 }
+console.log(f(10, 5), 20); // 20
+console.log(f(20, 20), 190); // 190
+console.log(f(15, 10), 60);// 60
+console.log(f(84939805, 28566164) === 1202653599565235);
+
+// console.log(84939805 / 28566164);
+console.log(f(84939805, 28566164));
+console.log(1202653599565235);
+
+// function f(n, m) {
+//   var wholeDiv = Math.floor(n / m);
+//   function sumOfModuloSeries(n, m) {
+//     var tmpSum = 0;
+//     while (true) {
+//       tmpSum += --n % m;
+//       if (n % m == 0) {break;}
+//     }
+//     return tmpSum;
+//   }
+//   return sumOfModuloSeries(n, m);
+// }
+// sumOfModuloSeries((n % m), m) +
