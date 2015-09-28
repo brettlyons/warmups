77eb133e01b452523faa9d4130ddbbce144bc20c
Brett Lyons <blyons@students.naropa.edu>
2015-09-28 16:00:35 -0600

    comments added

--- detectDuplicates.js
+++ detectDuplicates.js
@@ -1,5 +1,7 @@
 function isUniqueChars(string) {
   function go(string, cache) {
+    // O(1) lookup regardless of how big the cache {} gets
+    // this solution is inspired by memoization of recursive calls
     if (cache[string[0]]) { return false; }
     if (string.length == 0) { return true; }
     cache[string[0]] = true;
