fd8966aa59fe9500580599946a8270f0d7d2268e
Brett Lyons <blyons@students.naropa.edu>
2015-10-03 18:21:40 -0600

    Added some solutions to slope.clj, prob. going to rename

--- slope.clj
+++ slope.clj
@@ -5,3 +5,20 @@
     (str "undefined")
     (str (/ (- (first (rest v)) (last v)) (- (first v) (first (rest (rest v))))))))
 
+
+(ns waiting-room)
+
+(comment
+  small koans from a relatively well known code training website.
+)
+(defn last-chair [n]
+	(- n  1)
+)
+
+(ns penultimate)
+
+(defn penultimate
+  "Gets the second to last element of an ISeq"
+  [lst]
+	(second (reverse lst))
+)
