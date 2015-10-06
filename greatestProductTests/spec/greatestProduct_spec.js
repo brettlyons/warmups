0527b2b845a4ddfcaa3197b5b0bd2ade2a5ceefa
Brett Lyons <blyons@students.naropa.edu>
2015-10-06 09:41:50 -0600

    Set up tests for greatestProductTests

---
+++ greatestProductTests/spec/greatestProduct_spec.js
@@ -0,0 +1,17 @@
+var greatestProductOfPairs = require('../src');
+
+describe('greatestProductOfPairs', function() {
+  var input1 = [2,3,4,-5,92,100];
+  var input2 = [-2,3,-4,-5,-92,-100];
+  var input3 = [-2, -3, 5];
+  var emptyInput = [];
+  var nonNumberInput = 'Some string';
+  it('returns the greatest product of any pairs within an array', function() {
+    expect(greatestProductOfPairs(input1)).toEqual(9200);
+    expect(greatestProductOfPairs(input2)).toEqual(9200);
+    expect(greatestProductOfPairs(input3)).toEqual(6);
+  });
+  it('returns 0 if input is empty', function() {
+    expect(greatestProductOfPairs(emptyInput)).toEqual(0);
+  });
+});
---
+++ greatestProductTests/src.js
@@ -0,0 +1,31 @@
+// var ramda = require('ramda');
+
+function greatestProductOfPairs(array) {
+  if (array.length == 0) { return 0; }
+  var result = -Infinity;
+  // count = 0;// demo of memoization, aka DRY javascript interpreter.
+  //var mult = ramda.memoize(function (x,y) {
+  //count++;
+  //return x * y;
+  //});
+
+  var mult = function(x, y) {
+    // count++;
+    return x * y;
+  };
+
+  for (var i = 0 ; i < array.length ; i++) {
+    for (var j = i ; j < array.length ; j++) {
+      if (mult(array[ i ], array[ j ]) > result && i !== j) {
+        result = mult(array[ i ], array[ j ]);
+      }
+    }
+  }
+  // console.log('Multiply is run: ', count, ' times'); // side effect.
+  return result;
+}
+
+// console.log(greatestProductOfPairs([2,3,4,-5,92,100])); // 9200
+// console.log(greatestProductOfPairs([-2,3,-4,-5,-92,-100])); // 9200
+
+module.exports = greatestProductOfPairs;
