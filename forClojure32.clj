b2c56c91adbc69b8eb56b7020b28a7dbd5b21f17
Brett Lyons <blyons@students.naropa.edu>
2015-10-19 09:00:01 -0600

    Forgot to commit and push yesterday.

---
+++ forClojure32.clj
@@ -0,0 +1,26 @@
+(ns Duplicate)
+
+(comment
+(defn dupe [g]
+  (for [x [g]]
+   (fn [r x]
+     (println r x)
+     (apply conj r (repeat 2 x)))
+   []
+   g))
+)
+
+(defn dupe [g]
+  (reduce
+   (fn [r x] (apply conj r (repeat 2 x)))
+   []
+   g))
+
+(comment
+  "this works"
+  "the key was to use apply"
+  )
+
+
+(dupe [1 2 3])
+(dupe [[1 2][3 4]])
