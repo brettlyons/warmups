28a08f952aeb41923310d653d150155052db05da
Brett Lyons <blyons@students.naropa.edu>
2015-11-05 13:22:52 -0700

    Worked on lumberjackpiles and ascii house, missed a day

---
+++ lumberjackPiles.js
@@ -0,0 +1,51 @@
+// store log piles in an array of arrays.
+// so [[1 1 1],
+//     [2 1 3],
+//     [1 4 1]]
+//
+// is a 3x3 set of piles, and
+// and would result in . . .
+// [[3 2 2],
+//  [2 2 3],
+//  [2 4 2]]
+
+
+// function placeLogs(pilesArrays, remainingLogs) {
+//   var sndToLowest = 2;// magic number.
+//   for(var i = 0 ; i < pilesArrays.length ; i++ ) {
+//     for(var j = 0 ; j < pilesArrays[i].length ; j++ ) {
+//       if(pilesArrays[i][j] < sndToLowest) {
+//         pilesArrays[i][j] += 1;
+//         remainingLogs--;
+//       }
+//     }
+//   }
+// }
+var firstTestPiles = [[1,1,1],
+                      [2,1,3],
+                      [1,4,1]]
+
+function placeLogs(pilesArrays, remainingLogs, secondToLowest) {
+  var i = 0;
+  var j = 0;
+  while(remainingLogs > 0) {
+    if(pilesArrays[i][j] < secondToLowest) {
+      pilesArrays[i][j] += 1;
+      remainingLogs--;
+    }
+    if(j == pilesArrays[i].length) {
+      i++;
+      j = 0;
+    }
+    if(i == pilesArrays.length - 1 && j == pilesArrays[i].length - 1) {
+      secondToLowest++;
+      i = 0;
+    }
+    j++;
+  }
+}
+
+console.log(firstTestPiles);
+placeLogs(firstTestPiles, 7, 2);
+console.log(firstTestPiles);
+
