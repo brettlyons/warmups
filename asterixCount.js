2da3e4a7967c3c354369082035fe2b3b6f94779c
Brett Lyons <blyons@students.naropa.edu>
2015-11-06 09:50:48 -0700

    "finished" asterixCount.js

--- asterixCount.js
+++ asterixCount.js
@@ -1,16 +1,32 @@
-function countAsterix(inputString) {
-  return inputString.split('').reduce(function (count, currentChar) {
-    return (currentChar == '*') ? ++count : count;
-  }, 0);
+// this is a "I didn't want to re-write findBiggestCount to accomodate abstraction of selectThingToCount" function.
+function countAsterix(input) {
+  return selectThingToCount('*')(input);
 }
 
-function findBiggestCount(inputString) {
-  return inputString.split(' ').map(countAsterix).sort(largestSort)[0];
+
+function findBiggestCount(inputString, sortFn, splitChar) {
+  if(!splitChar) {
+    splitChar = ' ';
+  }
+  return inputString.split(splitChar).map(countAsterix).sort(sortFn)[0];
 }
+
 function largestSort(a,b) {
   return b - a;
+} // So you can also sort by smallest
+
+function smallestSort(a, b) {
+  return a - b;
 }
 
-console.log("3 ? ", findBiggestCount("*** *** **"));
-console.log("4 ? ", findBiggestCount("*** *** **** *"));
-console.log("2 ? ", findBiggestCount("* **"));
+function selectThingToCount(charToCount) {
+  return function (input) {
+    return input.split('').reduce(function (count, currentChar) {
+      return (currentChar == charToCount) ? ++count : count;
+    }, 0);
+  }
+}
+
+console.log("TEST: 3 ? ", findBiggestCount("*** *** **", largestSort));
+console.log("TEST: 4 ? ", findBiggestCount("*** *** **** *", largestSort));
+console.log("TEST: 2 ? ", findBiggestCount("* **", largestSort));
