69bd610cff170831d19b9e52236559fdc7ad2315
Brett Lyons <blyons@students.naropa.edu>
2015-11-16 09:17:37 -0700

    Made new sortprac.js file

---
+++ sortprac.js
@@ -0,0 +1,12 @@
+var testArray = [1, 3, 17, 4, 5, 2, 8, 9, 10, 12, 11, 6, 19, 7, 13, 18, 16, 15,
+                 14, 20];
+
+// console.log(testArray.sort(function(a, b) {
+//   return a - b;
+// }));
+
+console.log("Before ", testArray);
+testArray.sort(function(a, b) {
+  return a - b;
+});
+console.log("After ", testArray);
