65a6f120669571a28bc3564832f7db96b4f47af5
Brett Lyons <blyons@students.naropa.edu>
2015-12-03 09:26:51 -0700

    Redid permutations, again

--- permutations.clj
+++ permutations.clj
@@ -9,3 +9,11 @@
 
 (permutation str3 str2)
 (permutation str1 str2)
+
+(defn permutation? [str1 str2]
+  (and (= (count str1) (count str2))
+       (= (into #{} str1) (into #{} str2))))
+
+(permutation? str3 str2) ; should be => false
+(permutation? str1 str2) ; should be => true
+
