1d9088f15ad80d7fcbae44d626c3c3c4c7cc7234
Brett Lyons <blyons@students.naropa.edu>
2015-12-03 19:09:24 -0700

    Working on java-interview Q's

--- convol.js
+++ convol.js
@@ -1,9 +1,8 @@
-function convoluted(string) { var output = '';
-
+function convoluted(string) {
+  var output = '';
   for (var i = 0; i < string.length; i++) { output += ('a' + string.charAt(i)); }
-
   for (var i = 0; i < string.length; i++) { output += ('b' + string.charAt(i)); }
-
-return output; }
+  return output;
+}
 
 console.log(convoluted('double, trouple, toil and bubble'));
