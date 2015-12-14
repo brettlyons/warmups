57bfbc679bcb3c323d9d4ed9d4635bf6a5de5e3f
Brett Lyons <blyons@students.naropa.edu>
2015-12-14 15:50:38 -0700

    Finished linkedList stacks and queues

--- js-datastructures/linkedList.js
+++ js-datastructures/linkedList.js
@@ -16,19 +16,25 @@ function LinkedList(headData) {
 
 LinkedList.prototype.push = function (newData) {
   this.head = new Node(newData, this.head);
+  if(!this.last) { this.last = this.head; }
 };
 
 LinkedList.prototype.pop = function () {
+  if(!this.head) { return null; }
+
   var freedData = this.head.data;
   this.head = this.head.next;
+  if (!this.head) { this.last = null; }
   return freedData; 
 };
 
 LinkedList.prototype.addToTail = function (newData) {
-  this.last.next = new Node(newData, null);
-  this.last = this.last.next;
+  if(!this.last) { this.last = new Node(newData, null); }
+  else {
+    this.last.next = new Node(newData, null);
+    this.last = this.last.next;
+  }
 };
-
 
 var tmpLL = new LinkedList("one");
 // new up a linked list
@@ -46,3 +52,55 @@ console.log(JSON.stringify(tmpLL, null, 4));
 // add something to the tail of the list
 tmpLL.addToTail("pizza");
 console.log(JSON.stringify(tmpLL, null, 4));
+
+function Stack(firstVal) {
+  var that = this;
+  if(!firstVal) { throw "Stack requires an initial value."; }
+  this.innerLL = new LinkedList(firstVal, null);
+  return {
+    push: function (newData) {
+      that.innerLL.push(newData);
+    },
+    pop: function () {
+      var thing = that.innerLL.pop();
+      if(!thing) {return null;}
+      return thing;
+    }
+  };
+}
+// make a stack, push four things onto it, and pop them off
+var myStack = new Stack("one");
+myStack.push("two");
+myStack.push("three");
+myStack.push("four");
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+
+function Queue(firstVal) {
+  var that = this;
+  if(!firstVal) { throw "Queue requires an initial value."; }
+  this.innerLL = new LinkedList(firstVal, null);
+  return {
+    enqueue: function (newData) {
+      that.innerLL.addToTail(newData);
+    },
+    dequeue: function () {
+      var thing = that.innerLL.pop();
+      if(!thing) {return null;}
+      return thing;
+    }
+  };
+}
+// make a queue, put some things onto it, pop them off
+var myQueue = new Queue("one 5");
+myQueue.enqueue("two 10");
+myQueue.enqueue("three 239");
+myQueue.enqueue("four 29");
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
