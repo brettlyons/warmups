b5db61a65223d1348d54cc98f9b8dc3fe11d2a4f
Brett Lyons <blyons@students.naropa.edu>
2015-12-17 10:18:45 -0700

    Tail Call test changes

--- tailcalltest.js
+++ tailcalltest.js
@@ -3,8 +3,10 @@ function sum(n, acc) {
   return n == 0 ? acc : function() { return sum(n - 1, acc + n);  };
 }
 
-f = sum(1000000, 0);
+f = sum(1000000000, 0);
 while (typeof f == 'function') {
   f = f();
 }
 console.log(f);
+
+// console.log(sum(10000)); // <- breaks.
