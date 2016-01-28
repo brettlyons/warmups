afa5fdc8b9ac5184382bf9311c3710ae3d9a0b01
Brett Lyons <blyons@students.naropa.edu>
2016-01-27 21:06:48 -0700

    Moar work on clj fib list

--- fibo.clj
+++ fibo.clj
@@ -1,10 +1,23 @@
 (:ns fibonacci)
 
-(defn fibonacci [n & collection]
-  (let [collection (or collection ())]
-    (if (= (count collection) n)
-      collection
-      (recur (- n 1) (cons collection (+ (last collection) n))))))
+;; (defn fibonacci [n & collection]
+;;   (let [collection (or collection ())]
+;;     (if (= (count collection) n)
+;;       collection
+;;       (recur (- n 1) (cons collection (+ (last collection) n))))))
+;; (println (fibonacci 10))
 
-(println (fibonacci 10))
+(def m-fib
+  (memoize (fn [n]
+             (condp = n
+               0 1
+               1 1
+               (+ (m-fib (dec n)) (m-fib (- n 2)))))))
 
+(dotimes [n 5]
+  (println (m-fib n)))
+
+;; (defn fiblist [n]
+;;   (reduce #(conj % (+ %2 (last %))) [1 1]))
+
+;; (println (fiblist 2))
