29e8bf23427d0aaf396e9896bd662df699f82ffb
Brett Lyons <blyons@students.naropa.edu>
2016-02-04 19:59:03 -0700

    Got my-max working, solving the 4clojure problem

--- my-max.clj
+++ my-max.clj
@@ -1,13 +1,22 @@
 (ns my-max)
 
-(defn my-max
-  ([seq]
-   (my-max seq (first seq)))
-  ([seq current-max]
-   (if (empty? seq)
-     current-max
-     (if (> (first seq) current-max)
-       (recur (rest seq) (first seq))
-       (recur (rest seq) current-max)))))
+;; (defn my-max
+;;   ([seq & rest]
+;;    (my-max seq (first seq)))
+;;   ([seq current-max & rest]
+;;    (if (empty? seq)
+;;      current-max
+;;      (if (> (first seq) current-max)
+;;        (recur (rest seq) (first seq) rest)
+;;        (recur (rest seq) current-max rest)))))
 
-(println (my-max [1 2 3 4 8 4]))
+(defn my-max
+  [fir & rem]
+  (if (empty? rem)
+    fir
+    (if (> fir (first rem))
+      (recur fir (rest rem))
+      (recur (first rem) (rest rem)))))
+
+(println (my-max 1 2 3 4 8 4))
+(println (my-max 3 29 19 27))
