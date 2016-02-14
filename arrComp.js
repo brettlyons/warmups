b43ba8297eee27af657b71bc3db5d0189305a657
Brett Lyons <blyons@students.naropa.edu>
2016-02-13 20:26:20 -0700

    Fixed arrComp.js to ret false on array length mismatch

--- arrComp.js
+++ arrComp.js
@@ -2,6 +2,8 @@ var array1 = [ "cat", "dog", "fish" ];
 var array2 = [ "lion", "pointer", "trout" ];
 var array3 = [ "earth", "jupiter", "mars" ];
 var array4 = [ "earth", "jupiter", "mars" ];
+var array5 = [ "earth", "jupiter" ];
+
 
 var eq = function (thing1, thing2) {
   return thing1 === thing2;
@@ -9,7 +11,7 @@ var eq = function (thing1, thing2) {
 
 function configureCompareArrays(comparisonFn) {
   return function (inputArr1, inputArr2) {
-    for(var i = 0; i < inputArr1.length; i++) {
+    for(var i = 0; i < inputArr1.length || i < inputArr2.length ; i++) {
       if(!comparisonFn(inputArr1[i], inputArr2[i])) {
         return false;
       }
@@ -22,3 +24,4 @@ var compareArraysForEquality = configureCompareArrays(eq);
 
 console.log("false? => ", compareArraysForEquality(array1, array2));
 console.log("true? => ", compareArraysForEquality(array3, array4));
+console.log("false? => ", compareArraysForEquality(array4, array5));
