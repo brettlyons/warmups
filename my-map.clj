643e2e09afb02cedd50b2c0170769321d0ffc6fc
Brett Lyons <blyons@students.naropa.edu>
2016-02-20 21:30:15 -0700

    I don't get lazy sequences in this warmup.  Mahybe I try again tommorow

--- my-map.clj
+++ my-map.clj
@@ -3,7 +3,13 @@
 (defn my-map
   [fun sequ & total]
   (if (empty? sequ)
-    (reverse total)
+    (reverse (lazy-seq total))
     (recur fun (rest sequ) (cons (fun (first sequ)) total))))
 
+;; maybe try cons (lazy-seq (^^^...)) ?? 
 (println (my-map inc [1 2 3 4]))
+(println (time (my-map inc [1 2 3 4])))
+
+(println (time (->> (my-map inc (range))
+        (drop (dec 1000000))
+        (take 2))))
