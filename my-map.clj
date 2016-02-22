b7adb8c788d1820020944385eae7d638ec8d7380
Brett Lyons <blyons@students.naropa.edu>
2016-02-21 22:24:53 -0700

    Trying it in terms of reduce now

--- my-map.clj
+++ my-map.clj
@@ -1,10 +1,15 @@
 (ns my-map)
 
+;; (defn my-map
+;;   [fun sequ & total]
+;;   (if (empty? sequ)
+;;     (reverse (lazy-seq total))
+;;     (recur fun (rest sequ) (cons (fun (first sequ)) total))))
+
 (defn my-map
-  [fun sequ & total]
-  (if (empty? sequ)
-    (reverse (lazy-seq total))
-    (recur fun (rest sequ) (cons (fun (first sequ)) total))))
+  [fun sequ]
+  (lazy-seq (reduce (fn [accum current]
+                      (cons accum (fun current))) sequ)))
 
 ;; maybe try cons (lazy-seq (^^^...)) ?? 
 (println (my-map inc [1 2 3 4]))
