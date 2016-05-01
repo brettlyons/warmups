eab3fb8e92576feabaae64d97533db39b06e645e
Brett Lyons <blyons@students.naropa.edu>
2016-05-01 15:00:21 -0600

    Yet another vers. of fizzbuzz, using tuples this time.

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -11,17 +11,17 @@ defmodule FizzbuzzModule do
   #end
   # doesn't quite work -- maybe poor fn description?
 
-  def fizzbuzz(0, 0, _), do: "FizzBuzz"
-  def fizzbuzz(0, _, _), do: "Fizz"
-  def fizzbuzz(_, 0, _), do: "Buzz"
-  def fizzbuzz(_, _, n), do: n
+  def fizzbuzz({0, 0, _}), do: "FizzBuzz"
+  def fizzbuzz({0, _, _}), do: "Fizz"
+  def fizzbuzz({_, 0, _}), do: "Buzz"
+  def fizzbuzz({_, _, n}), do: n
 
   def do_fizzbuzz(_, 0) do # end on 0
     IO.puts(" THE END ")
   end
 
   def do_fizzbuzz(n, ending) do
-    IO.puts(fizzbuzz(rem(n,3), rem(n,5), n))
+    IO.puts(fizzbuzz({rem(n,3), rem(n,5), n}))
     #IO.puts(fizzbuzz_two(rem(n,3), rem(n,5), n))
     do_fizzbuzz(n+1, ending-1)
   end
