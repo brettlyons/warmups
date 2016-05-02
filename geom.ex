c411f43741bdafcaee9d1a30297ca8dadabfad6a
Brett Lyons <blyons@students.naropa.edu>
2016-05-02 09:20:04 -0600

    Working on elixir etudes, with geom.ex

--- geom.ex
+++ geom.ex
@@ -7,12 +7,24 @@ defmodule Geom do
 
   @doc """
   Given two values, calculates the area as if they were the length and width of
-  a rectangle.
+  a <atom> where atom is either :rectangle, :triangle, or :ellipse.
   """
 
-  @spec area(number(), number()) :: number()
+  @spec area(atom(), number(), number()) :: number()
 
-  def area(x \\ 1,y \\ 1) do
+  def area(:rectangle, x, y) when x > 0 and y > 0 do
     x * y
+  end
+
+  def area(:triangle, x, y) when x > 0 and y > 0 do
+    x * y / 2.0
+  end
+
+  def area(:ellipse, x, y) when x > 0 and y > 0 do
+    :math.pi * x * y
+  end
+
+  def area(_, x, y) do # should let it fail...
+    0
   end
 end
