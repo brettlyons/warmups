47d209f009970f215f16fead8c33d5b7ef92a5cf
Brett Lyons <blyons@students.naropa.edu>
2017-05-10 22:14:04 -0600

    Cleanup, completion of averages and comparison of d6 to d10 and d12

--- dndgreatweaponfightingtest.js
+++ dndgreatweaponfightingtest.js
@@ -1,12 +1,11 @@
-
-function attackRoll() {
-    return Math.floor(Math.random() * 6) + 1;
+function attackRoll(dice_size) {
+    return Math.floor(Math.random() * dice_size) + 1;
 }
 
-function generateRollset(size) {
+function generateRollset(dice_size, size) {
     var rolls = Array(size);
     for(var i = 0; i < size; i++) {
-        rolls[i] = (attackRoll());
+        rolls[i] = (attackRoll(dice_size));
     }
     return rolls;
 }
@@ -15,24 +14,36 @@ function sum(a, b) {
     return a + b;
 }
 
-function reRollLow(roll) {
+function reRollLow(dice_size, roll) {
     if(roll < 3) {
-        return attackRoll();
+        return attackRoll(dice_size);
     }
     else {
         return roll;
     }
 }
 
-function varianceTrial(rolls) {
-    return rolls.map(reRollLow).reduce(sum) - rolls.reduce(sum);
+function varianceTrial(dice_size, rolls) {
+    var reRollLowOne = function(roll) { return reRollLow(dice_size, roll); };
+    return rolls.map(reRollLowOne).reduce(sum) - rolls.reduce(sum);
 }
-var varianceList = [];
-for(var i = 1; i < 10001; i++) {
-    varianceList.push(varianceTrial(generateRollset(10)));
-}
-console.log("average extra damage total from d6's per combat encounter from great weapon fighting style assuming 10 damage rolls per encounter");
-console.log((varianceList.reduce(sum) / 10000));
 
+function generateFullInfo(dice_max, dmg_rolls_per_encounter) {
+    var varianceList = [];
+    var rollset_total = [];
+    for(var i = 1; i < 10001; i++) {
+        var rollset = generateRollset(dice_max, dmg_rolls_per_encounter);
+        varianceList.push(varianceTrial(dice_max, rollset));
+        rollset_total.push(rollset.reduce(sum));
+    }
+    console.log("average extra damage total from d" + dice_max + "'s per combat encounter from great weapon fighting style assuming " + dmg_rolls_per_encounter + " damage rolls per encounter");
+    console.log((varianceList.reduce(sum) / 10000));
+    console.log("average damage per encounter with d" + dice_max);
+    console.log(rollset_total.reduce(sum) / 10000);
+
+}
+generateFullInfo(6, 10);
+generateFullInfo(12, 5);
+generateFullInfo(10, 5);
 
 
